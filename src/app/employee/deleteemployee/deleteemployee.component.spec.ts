baf83da3bfe41736e21ffd7af6b669d0e335faf5
Vinayak Panamd <maheshbhosale939@gmail.com>
2020-12-03 19:12:45 +0530

    deploye heroku

--- src/app/employee/deleteemployee/deleteemployee.component.spec.ts
+++ src/app/employee/deleteemployee/deleteemployee.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { DeleteemployeeComponent } from './deleteemployee.component';
 
@@ -6,7 +6,7 @@ describe('DeleteemployeeComponent', () => {
   let component: DeleteemployeeComponent;
   let fixture: ComponentFixture<DeleteemployeeComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ DeleteemployeeComponent ]
     })
--- src/app/employee/employeeadd/employeeadd.component.spec.ts
+++ src/app/employee/employeeadd/employeeadd.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { EmployeeaddComponent } from './employeeadd.component';
 
@@ -6,7 +6,7 @@ describe('EmployeeaddComponent', () => {
   let component: EmployeeaddComponent;
   let fixture: ComponentFixture<EmployeeaddComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ EmployeeaddComponent ]
     })
